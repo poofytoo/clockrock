2629684de7636cb1a44abb6c658c4bc96de284e2
Stephanie Yu <styu314@gmail.com>
2014-10-05 03:06:49 -0400

    add button

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -1,12 +1,5 @@
-// Your client side JavaScript here
-navigator.vibrate = navigator.vibrate ||
-                  navigator.webkitVibrate ||
-                  navigator.mozVibrate || 
-                  navigator.msVibrate;
-
-if (navigator.vibrate) {
-  $('#test').text('we can vibrate');
-  navigator.vibrate(500);
-} else {
-  $('#test').text('no vibration for you :-(');
-}
+$('#toggle').click(function() {
+  $.post('/toggle', {}, function(data) {
+    console.log(data);
+  });
+})
