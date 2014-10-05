d8810c71eb0a0993875b3879ea830fee875a739b
poofytoo <victorhung92@gmail.com>
2014-10-05 04:14:08 -0400

    firebase

--- node/app.js
+++ node/app.js
@@ -6,6 +6,7 @@ var hbs = require('hbs');
 var routes = require(__dirname + '/routes/routes');
 var authConfig = require(__dirname + '/authConfig');
 var WinkAPI = require(__dirname + '/winkapi');
+var Firebase = require("firebase");
 
 var app = express();
 var toggleStatus = false;
@@ -25,45 +26,57 @@ winkapi = new WinkAPI.WinkAPI({ clientID     : authConfig.auth.client_id
                               , clientSecret : authConfig.auth.client_secret }).login(
                               authConfig.auth.username, authConfig.auth.password, function(err) {
   if (!!err) return console.log('login error: ' + err.message);
-
   // otherwise, good to go!
-
   winkapi.setOutlet(20582, !toggleStatus, function(err, outlet) {
     if (!!err) return console.log('setOutlet: ' + err.message);
   });
-  var dial = {};
 
-  dial.path = '/dials/33321';
-  
-  dial.props =      {
-      "dial_template_id": "10",
-      "dial_configuration": {
-          "min_value": 0,
-          "max_value": 360,
-          "min_position": 0,
-          "max_position": 360,
-          "scale_type": "linear",
-          "rotation": "cw",
-          "num_ticks": 12
-      },
-      "channel_configuration": {
-          "channel_id": "10"
-      },
-      "name": "Manual control",
-      "value": 300,
-      "labels": ["hi", "hi2"]
-  }
-  
-  winkapi.setDial(dial, { enabled: true }, function(err, dial) {
-    if (!!err) return console.log('setDial: ' + err.message);
-    // inspect dial{}
-  });
-
+  init();
 
 }).on('error', function(err) {
   console.log('background error: ' + err.message);
 });
 
+init = function() {
+  var fref = new Firebase("https://poofytoo.firebaseio.com/nimbus");
+
+  fref.on('value', function(snapshot) {
+    var data = snapshot.val();
+
+    for (i = 1; i < 5; i++) {
+      var dial = {};
+      dial.path = '/dials/3332' + i;
+      dial.props =      {
+            "dial_template_id": "10",
+            "dial_configuration": {
+                "min_value": 0,
+                "max_value": 360,
+                "min_position": 0,
+                "max_position": 360,
+                "scale_type": "linear",
+                "rotation": "cw",
+                "num_ticks": 12
+            },
+            "channel_configuration": {
+                "channel_id": "10"
+            },
+            "name": "Manual control",
+            "value": data[i].pointer,
+            "label": data[i].label
+        }
+      winkapi.setDial(dial, { enabled: true }, function(err, dial) {
+        if (!!err) return console.log('setDial: ' + err.message);
+        // inspect dial{}
+      });
+      }
+      /*
+*/
+
+  })
+  
+  
+
+}
 
 
 app.get('/', routes.index);
